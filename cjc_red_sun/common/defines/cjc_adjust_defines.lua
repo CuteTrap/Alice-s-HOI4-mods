09b0576bc5958e1e008f870a4d01b0b7e90eb4d6
CuteTrap <70308762+CuteTrap@users.noreply.github.com>
2022-03-25 12:22:55 -0500

    Add files via upload

---
+++ cjc_red_sun/common/defines/cjc_adjust_defines.lua
@@ -0,0 +1,6 @@
+NDefines.NOperatives.CONTROL_TRADE_MAX_INFLUENCE = 200.0;					-- The maximum amount of trade influence that can be gained through the control trade mission
+NDefines.NCountry.MAX_PROPAGANDA_STABILITY_IMPACT = -0.4;			-- Max total penalty from operative performing the propaganda mission in a country
+NDefines.NCountry.MAX_PROPAGANDA_WAR_SUPPORT_IMPACT = -0.4;		-- Max total penalty from operative performing the propaganda mission in a country
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1000;			--Max army experience a country can store
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1000;			--Max navy experience a country can store
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1000;				--Max air experience a country can store
